71e4b9d8dfa6a312f41adfd5daa1c7adba75d67e
Vallista <0x109071072@gmail.com>
2024-01-28 02:14:07 +0900

    feature: build 커맨드 추가

--- scripts/cli/index.mjs
+++ scripts/cli/index.mjs
@@ -6,6 +6,7 @@ import yargs from 'yargs/yargs'
 
 import dev from './dev.mjs'
 import start from './start.mjs'
+import build from './build.mjs'
 
 const shortshands = {
   dev: 'development',
@@ -28,6 +29,7 @@ yargs(hideBin(process.argv))
   .scriptName('cli')
   .command('dev [env] [local]', '개발 환경을 실행합니다.', setPositionalEnv, (argv) => dev(argv.env, argv))
   .command('start [env] [local]', '어플리케이션을 실행합니다.', setPositionalEnv, (argv) => start(argv.env, argv))
+  .command('build [env] [local]', '라이브러리를 빌드합니다.', setPositionalEnv, (argv) => build(argv.env, argv))
   .choices('env', ['dev', 'development', 'prod', 'production'])
   .help().argv
 
